152e925b6f1b60391d6fc2800380e148ba63283e
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-10-01 13:23:31 -0400

    Update

--- Project/Mathlib/Algebra/Torsor/Defs.lean
+++ Project/Mathlib/Algebra/Torsor/Defs.lean
@@ -1,13 +1,13 @@
 import Mathlib.Algebra.Group.Action.Pretransitive
 
-class AddTorsor
+class IsAddTorsor
     (G : Type*) [AddGroup G] (F : Type*) [AddAction G F]
     extends AddAction.IsPretransitive G F where
   nonempty : Nonempty F
   free : ∀ (f : F) (g : G), g +ᵥ f = f → g = 0
 
 @[to_additive (attr := mk_iff)]
-class MulTorsor
+class IsMulTorsor
     (G : Type*) [Group G] (F : Type*) [MulAction G F]
     extends MulAction.IsPretransitive G F where
   nonempty : Nonempty F
--- Project/Mathlib/Topology/GBundle/Defs.lean
+++ Project/Mathlib/Topology/GBundle/Defs.lean
@@ -1,18 +1,11 @@
 import Mathlib.Topology.FiberBundle.Basic
-import Mathlib.Algebra.Group.Action.Defs
-import Mathlib.Topology.Algebra.MulAction
-
-open Bundle
+import Project.Mathlib.Algebra.Torsor.Defs
 
 structure GBundleCore
     (ι : Type*) (B : Type*) [TopologicalSpace B]
     (F : Type*) [TopologicalSpace F]
     (G : Type*) [Group G]
-    [MulAction G F] [MulAction.IsPretransitive G F]
+    [MulAction G F] [IsMulTorsor G F]
     extends FiberBundleCore ι B F where
   coordChange_structure_group :
     ∀ i j, ∀ x ∈ baseSet i ∩ baseSet j, ∃ g : G, ∀ v : F, coordChange i j x v = g • v
-  /- respects_fibres : ∀ (p : TotalSpace F (core.Fiber)) (g : G), core.proj (p <• g) = core.proj p
-  (is_free : ∀ (p : TotalSpace F (core.Fiber)) (g : G), p <• g = p → g = 1)
-  (is_transitive : ∀ (p q : TotalSpace F (core.Fiber)),
-    core.proj p = core.proj q → ∃ g : G, p <• g = q) -/

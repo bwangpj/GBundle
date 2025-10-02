cac88e1e22c59afbfea4af4bac4460b355403041
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-10-01 23:06:44 -0400

    Update Defs.lean

--- Project/Mathlib/Algebra/Torsor/Defs.lean
+++ Project/Mathlib/Algebra/Torsor/Defs.lean
@@ -1,4 +1,6 @@
 import Mathlib.Algebra.Group.Action.Pretransitive
+import Mathlib.Algebra.AddTorsor.Defs
+import Mathlib.GroupTheory.GroupAction.Hom
 
 class IsAddTorsor
     (G : Type*) [AddGroup G] (F : Type*) [AddAction G F]
@@ -12,3 +14,28 @@ class IsMulTorsor
     extends MulAction.IsPretransitive G F where
   nonempty : Nonempty F
   free : ∀ (f : F) (g : G), g • f = f → g = 1
+
+instance AddTorsor.isAddTorsor {G : Type*} [AddGroup G] {F : Type*} [AddTorsor G F] :
+    IsAddTorsor G F where
+  exists_vadd_eq x y := ⟨y -ᵥ x, vsub_vadd' _ _⟩
+  nonempty := nonempty
+  free f g h := (vsub_self f) ▸ (h ▸ (vadd_vsub' g f)).symm
+
+@[to_additive]
+noncomputable def IsMulTorsor.equiv_group (G : Type*) [Group G] (F : Type*)
+    [MulAction G F] [IsMulTorsor G F]
+    (b : F) :
+    F ≃ G where
+  toFun f := (toIsPretransitive.exists_smul_eq b f).choose
+  invFun g := g • b
+  left_inv := by
+    refine Function.leftInverse_iff_comp.mpr ?_
+    ext f; simpa using (toIsPretransitive.exists_smul_eq b f).choose_spec
+  right_inv := by
+    refine Function.rightInverse_iff_comp.mpr ?_
+    ext g; simp only [Function.comp_apply, id_eq]
+    apply inv_mul_eq_one.mp
+    apply free b _
+    rw [mul_smul]
+    exact inv_smul_eq_iff.mpr
+      (toIsPretransitive.exists_smul_eq b (g • b)).choose_spec.symm
